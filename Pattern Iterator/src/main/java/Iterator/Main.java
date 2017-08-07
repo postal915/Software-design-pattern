9727b8ac6ddb41a23c5269b99446feebb020f88b
Stas Latyshev <postal915@gmail.com>
2017-08-07 16:40:42 +0300

    add pattern Iterator

---
+++ Pattern Iterator/src/main/java/Iterator/Main.java
@@ -0,0 +1,14 @@
+package Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        NameRepository nameRepository = new NameRepository();
+        Iterator iterator = nameRepository.getIterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
