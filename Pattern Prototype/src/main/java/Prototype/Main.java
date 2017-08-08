485849e8245cf6fa180e665a3f4e496ec52ac600
Stas Latyshev <postal915@gmail.com>
2017-08-08 14:33:36 +0300

    add pattern Prototype

---
+++ Pattern Prototype/src/main/java/Prototype/Main.java
@@ -0,0 +1,30 @@
+package Prototype;
+
+public class Main {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Cache cache = new Cache();
+        cache.setStudent(new Student());
+        Student student = cache.getStudent();
+    }
+}
+
+class Student implements Cloneable {
+
+    @Override
+    public Student clone() throws CloneNotSupportedException {
+        return (Student) super.clone();
+    }
+}
+
+class Cache {
+
+    private Student student;
+
+    public Student getStudent() throws CloneNotSupportedException {
+        return student.clone();
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+}
