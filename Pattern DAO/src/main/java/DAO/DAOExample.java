c78201263960318b11f652cebbcf54230efebb9c
Stas Latyshev <postal915@gmail.com>
2017-08-19 16:08:49 +0300

    add pattern DAO (Data Access Object)

---
+++ Pattern DAO/src/main/java/DAO/DAOExample.java
@@ -0,0 +1,19 @@
+package DAO;
+
+public class DAOExample {
+    public static void main(String[] args) {
+
+        StudentDAO studentDAO = new StudentDAOImpl();
+
+        for (Student student : studentDAO.getAllStudents()) {
+            System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
+        }
+
+        Student student = studentDAO.getAllStudents().get(0);
+        student.setName("Michael");
+        studentDAO.updateStudent(student);
+
+        studentDAO.getStudent(0);
+        System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
+    }
+}
