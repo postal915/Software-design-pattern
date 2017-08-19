e9e9809f83bcca89a48632fa8586f2a7d48ccc5c
Stas Latyshev <postal915@gmail.com>
2017-08-19 16:08:29 +0300

    add pattern Composite Entity

---
+++ Pattern Composite Entity/src/main/java/Composite/Entity/CompositeEntityExample.java
@@ -0,0 +1,12 @@
+package Composite.Entity;
+
+public class CompositeEntityExample {
+    public static void main(String[] args) {
+
+        Client client = new Client();
+        client.setData("Test", "Data");
+        client.printData();
+        client.setData("Second Test", "Data1");
+        client.printData();
+    }
+}
