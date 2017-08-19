d10cb983694ccdb9974b131483c15cdfa2111ce8
Stas Latyshev <postal915@gmail.com>
2017-08-19 16:06:25 +0300

    add pattern Null Object

---
+++ Pattern Null Object/src/main/java/Null/Object/AbstractCustomer.java
@@ -0,0 +1,11 @@
+package Null.Object;
+
+public abstract class AbstractCustomer {
+
+    protected String name;
+
+    public abstract boolean isNil();
+
+    public abstract String getName();
+
+}
---
+++ Pattern Null Object/src/main/java/Null/Object/NullCustomer.java
@@ -0,0 +1,14 @@
+package Null.Object;
+
+public class NullCustomer extends AbstractCustomer {
+
+    @Override
+    public boolean isNil() {
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return "Not Available in Customer Database";
+    }
+}
