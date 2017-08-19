da3a1a29e31850ebea2584ed9b3fec9a1278b2ec
Stas Latyshev <postal915@gmail.com>
2017-08-19 16:00:04 +0300

    add pattern Interpreter

---
+++ Pattern Interpreter/src/main/java/Interpreter/InterpreterExample.java
@@ -0,0 +1,26 @@
+package Interpreter;
+
+public class InterpreterExample {
+    public static void main(String[] args) {
+
+        Expression isMale = getMaleExpression();
+        Expression isMarriedWoman = getMarriedExpression();
+
+        System.out.println("John is male? " + isMale.interpret("John"));
+        System.out.println("Julie is a married women? " + isMarriedWoman.interpret("Married Julie"));
+
+
+    }
+
+    public static Expression getMaleExpression() {
+        Expression robert = new TerminalExpression("Robert");
+        Expression john = new TerminalExpression("John");
+        return new OrExpression(robert, john);
+    }
+
+    public static Expression getMarriedExpression() {
+        Expression julie = new TerminalExpression("Julie");
+        Expression married = new TerminalExpression("Married");
+        return new AndExpression(julie, married);
+    }
+}
