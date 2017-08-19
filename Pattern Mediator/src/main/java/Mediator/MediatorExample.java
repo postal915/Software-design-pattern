39fc477917f490bc7d413591c0f1c608d8e50158
Stas Latyshev <postal915@gmail.com>
2017-08-19 16:00:45 +0300

    add pattern Mediator

---
+++ Pattern Mediator/src/main/java/Mediator/MediatorExample.java
@@ -0,0 +1,12 @@
+package Mediator;
+
+public class MediatorExample {
+    public static void main(String[] args) {
+
+        User robert = new User("Robert");
+        User john = new User("John");
+
+        robert.sendMessage("Hi! John!");
+        john.sendMessage("Hello! Robert!");
+    }
+}
